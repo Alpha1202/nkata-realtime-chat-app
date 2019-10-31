f30ab95909d458af7524ac49ce6676c8937d35e3
Nzubechukwu <nzubennamani@gmail.com>
2019-11-01 00:35:18 +0100

    integrate socket.io

--- server/index.js
+++ server/index.js
@@ -11,6 +11,14 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('We have a new connection');
+
+    socket.on('disconnect', () => {
+        console.log('User just left')
+    })
+})
+
 app.use(router); 
 
 server.listen(Port, () => console.log(`server is running on port ${Port}`))
