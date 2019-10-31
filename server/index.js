8954c28fc162fe928042b632f23e7f24a41f6c28
Nzubechukwu <nzubennamani@gmail.com>
2019-11-01 00:27:13 +0100

    setup a get homepage router

--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const http = require('http');
 
 const Port = process.env.PORT || 5000
 
+const router = require('./router')
+
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server);
+
+app.use(router); 
 
 server.listen(Port, () => console.log(`server is running on port ${Port}`))
