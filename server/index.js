e27f915086215b09ea23d7804a21539a006be18e
Nzubechukwu <nzubennamani@gmail.com>
2019-11-01 17:17:44 +0100

    add connect and disconnect users

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('We have a new connection');
 
+    socket.on('join', ({ name, join }, callback) => {
+
+    })
+
     socket.on('disconnect', () => {
         console.log('User just left')
     })
