c3bfd7d621a06c8ab58c207a52fccdb6a69eedae
Nzubechukwu <nzubennamani@gmail.com>
2019-11-02 16:50:32 +0100

    add admin messages add event to listen for user messages

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+
 const Port = process.env.PORT || 5000
 
 const router = require('./router')
@@ -12,11 +14,32 @@ const server = http.createServer(app)
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection');
-
     socket.on('join', ({ name, join }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
 
+        if(error) return callback(error);
+
+        // admin welcome message
+        socket.emit("message", { user: "admin", text: `${user.name} welcome to the room ${user.room}`})
+
+        // send an admin broadcast to everyone in the room
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined`});
+
+        // join a user to a room
+        socket.join(user.room)
+
+        callback();
     })
+
+    // events for user generated messages
+     socket.on('sendMessage', (message, callback) => {
+         const user = getUser(socket.id);
+
+         io.to(user.room).emit('message', {user: user.name, text: message});
+
+         callback();
+     })
+
 
     socket.on('disconnect', () => {
         console.log('User just left')
