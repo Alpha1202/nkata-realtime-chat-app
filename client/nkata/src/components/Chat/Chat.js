6507d47aac76a2e3a68baa69d4cb6fde256db9cc
Nzubechukwu <nzubennamani@gmail.com>
2019-11-03 12:59:17 +0100

    deploy apps to heroku and netlify

--- client/nkata/src/components/Chat/Chat.js
+++ client/nkata/src/components/Chat/Chat.js
@@ -17,8 +17,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
-//   const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+  // const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://nkata-chat-app.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -26,7 +26,7 @@ const Chat = ({ location }) => {
     socket = io(ENDPOINT);
 
     setRoom(room);
-    setName(name)
+    setName(name) 
 
     socket.emit('join', { name, room }, (error) => {
       if(error) {
